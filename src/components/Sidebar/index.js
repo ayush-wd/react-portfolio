851fea116bf5e78bfd2ea6fbeb62264a91edff80
Ayush yadav <20ayush07yadav03@gmail.com>
2022-08-01 23:53:54 +0530

    first-commit

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,20 @@
+
+import './index.css'
+import './index.css.map'
+import { Link } from 'react-router-dom'
+import Logo1 from '../../assets/images/logo1.png'
+
+
+
+const Sidebar = () => {
+    <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            {/* <img src={Logo1} alt="logo" /> */}
+        </Link>
+
+    </div>
+
+}
+
+
+export default Sidebar;
