9f2712625f8afddb12ff9bbc82b2132f243423e4
Ayush yadav <20ayush07yadav03@gmail.com>
2022-08-02 23:52:35 +0530

    sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,18 +1,21 @@
 
-import './index.css'
-import './index.css.map'
+
 import { Link } from 'react-router-dom'
 import Logo1 from '../../assets/images/logo1.png'
-
-
+import './App.css';
 
 const Sidebar = () => {
-    <div className='nav-bar'>
+    
+    
+    <div>
         <Link className='logo' to='/'>
             {/* <img src={Logo1} alt="logo" /> */}
         </Link>
 
     </div>
+
+    
+  
 
 }
 
